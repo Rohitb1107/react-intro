bf42a2c68e58394f9f5f0952ad0e5a56a6246602
Rohit <95957671+Rohitb1107@users.noreply.github.com>
2022-03-03 23:02:41 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const data = ["Android", "Blackberry", "iphone", "Windows Phone"];
+  const data1 = ["Samsung", "HTC", "Micromax", "Apple"];
   return (
-    <div className="App">
-     <h1>Hello Masai School</h1>
+    <div>
+      <ul>
+        <h1> Mobile Operating System{" "}</h1>
+        {data.map((e) => (
+          <li>{e}</li>
+        ))}
+      </ul>
+      <ul>
+        <h1> Mobile Manufactures{" "}</h1>
+        {data1.map((e) => (
+          <li>{e}</li>
+        ))}
+      </ul>
     </div>
   );
 }
+
 
 export default App;
