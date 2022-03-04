db6c3c1a504911aff8f00c51a32675db9a497a93
Rohit <95957671+Rohitb1107@users.noreply.github.com>
2022-03-04 14:29:31 +0530

    some changes 2

--- src/App.jsx
+++ src/App.jsx
@@ -22,5 +22,26 @@ function App() {
   );
 }
 
+// function App() {
+//   const data = ["Android", "Blackberry", "iphone", "Windows Phone"];
+//   const data1 = ["Samsung", "HTC", "Micromax", "Apple"];
+//   return (
+//     <div>
+//       <ul>
+//         <h1> Mobile Operating System{" "}</h1>
+//         {data.map((e) => (
+//           <li>{e}</li>
+//         ))}
+//       </ul>
+//       <ul>
+//         <h1> Mobile Manufactures{" "}</h1>
+//         {data1.map((e) => (
+//           <li>{e}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
 
 export default App;
